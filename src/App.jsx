a7f2e097a3ac1841534ad4652108850431c7442b
Phanindra <phanindragadireddy2@gmail.com>
2025-04-02 13:58:54 +0530

    chat bot ui created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './components/sidebar/Sidebar'
+import Mainpage from './components/Main/Mainpage'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Mainpage />
+    </>
+  )
+}
+
+export default App
