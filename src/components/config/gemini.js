3b805c89d65f1009610a0b6d572853f48ad1c293
Phanindra <phanindragadireddy2@gmail.com>
2025-04-03 16:16:59 +0530

    chat bot back created

--- src/components/config/gemini.js
+++ src/components/config/gemini.js
@@ -4,12 +4,12 @@ import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({ apiKey: "AIzaSyBl_X8uHlzJ3wkLs1sA526wrfq2FLh7HJM" });
 
-export default async function main() {
+export default async function main(prompt) {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents:"What is react js" ,
+    contents:prompt ,
   });
   console.log(response.text);
+  return response.text
 }
 
-await main();
